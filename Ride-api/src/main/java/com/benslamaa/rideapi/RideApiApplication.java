7d0115e9089b7d1b1308a7e9e52888be9f59569e
benslama abdulwahab <abdulwahab.benslama@zohomail.com>
2024-08-16 17:40:29 +0100

    Initialized a new Spring Boot project with foundational API setup replacing Symfony.
    Project Migration: Replaced the previous Symfony setup with a new Spring Boot project

---
+++ Ride-api/src/main/java/com/benslamaa/rideapi/RideApiApplication.java
@@ -0,0 +1,13 @@
+package com.benslamaa.rideapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RideApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RideApiApplication.class, args);
+	}
+
+}
